0a92ee631af81a2c435b7f44fdd3e3e60ceeffb0
Zsuzsanna Dianovics <31230409+deverzs@users.noreply.github.com>
2018-11-20 12:04:31 -0800

    Update Driver.java
    Fixed end of line drop

--- src/edu/miracosta/cs113/Driver.java
+++ src/edu/miracosta/cs113/Driver.java
@@ -1,69 +1,119 @@
 package edu.miracosta.cs113;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Driver {
 
     public static final int BITS_PER_CHAR = 16 ;
     public static void main (String args[]) {
 
-        String input = "hello";
-        String url = "http://www.zsusveganpantry.com/p/menu-planning.html";
-        boolean done = false ;
-        String outputTextFile = "text.txt"; // original file
+        String url =  "http://www.zsusveganpantry.com/p/menu-planning.html";   //"https://kbroman.org/simple_site/" ;
+
+        // file names
+        String originalFile = "original.txt";
+        String encodedFile = "encoded.txt" ;
+        String decodedFile = "decoded.txt" ;
+
+        // Bufferedreader, Printwriter & Stringbuilder
         BufferedReader br ;
+        BufferedReader br2 ;
+        PrintWriter toEncodedFile = null ;
+        PrintWriter toDecodedFile = null ;
+        StringBuilder sb = null ;
+        StringBuilder sb2 = null ;
+
+        // text variables
         String temp ;
         String cleanLine ;
+        String encodedText ;
+        String decodedText ;
+
+        // bit counts
         int originalBitCount = 0 ;
         int decodedBitCount = 0 ;
         int encodedBitCount = 0 ;
 
+
+        // Generating the Original text file using URL
         TextFileGenerator tfg = new TextFileGenerator() ;
-        StringBuilder sb = new StringBuilder() ;
+        sb = new StringBuilder() ;
+        sb2 = new StringBuilder() ;
+
         try {
+            tfg.makeCleanFile(url,originalFile );
 
-            tfg.makeCleanFile(url,outputTextFile );
-            originalBitCount = tfg.getNumChars(outputTextFile) * BITS_PER_CHAR ;
-            br = new BufferedReader(new FileReader(outputTextFile)) ;
-            while (!done) {
+            // original text bit count
+            originalBitCount = tfg.getNumChars(originalFile) * BITS_PER_CHAR ;
+
+            // reading original file
+            br = new BufferedReader(new FileReader(originalFile)) ;
+            temp = br.readLine() ;
+            while (temp != null) {
+                cleanLine = tfg.cleanString(temp);
+                sb.append(cleanLine + "\n") ;
                 temp = br.readLine();
-                if (temp!=null) {
-                    cleanLine = tfg.cleanString(temp);
-                    sb.append(cleanLine) ;
-                }
-
-                if (br.readLine()==null ){
-                    done = true ;
-                }
-
             }
+            br.close() ;
         }
         catch (IOException e) {
             System.out.println("try again");
         }
 
-        HuffmanTree huffy = new HuffmanTree(sb.toString()); // using original website text
+        // Building HuffmanTree, using original text
+        HuffmanTree huffy = new HuffmanTree(sb.toString());
 
         // create encoded file of 0's and 1's
+        try {
+            toEncodedFile = new PrintWriter(new FileOutputStream("encoded.txt"));
+            encodedText = huffy.encode(sb.toString()) ;
+            toEncodedFile.println(encodedText);
+            toEncodedFile.close();
+        }
+        catch (FileNotFoundException ex) {
+            System.err.println("Could not open output file " + encodedFile);
+            System.exit(0);
+        }
 
-        // create decoded file of back to text, original
+        // reading encoded file to grab String of code
+        try {
+            br2 = new BufferedReader(new FileReader(encodedFile)) ;
+            temp = br2.readLine() ;
+            while (temp != null) {
+                sb2.append(temp) ;
+                temp = br2.readLine();
+            }
+            br2.close() ;
 
-       // System.out.println( huffy.decode(input)) ;
+        } catch (IOException ex) {
+            System.err.println("Could not open output file " + encodedFile);
+            System.exit(0);
+        }
+
+        // translating code to text
+        try {
+            toDecodedFile = new PrintWriter(new FileOutputStream(decodedFile));
+            decodedText = huffy.decode(sb2.toString()) ;
+            toDecodedFile.print(decodedText);
+            toDecodedFile.close();
+        }
+        catch (FileNotFoundException ex) {
+            System.err.println("Could not open output file " + encodedFile);
+            System.exit(0);
+        }
+
         // Original bit count:
-        System.out.println("Original bit count is: " + originalBitCount);
+        System.out.printf("Original bit count is: %,d \n",  originalBitCount ) ;
 
         // Decoded bit count, should be same as original
         decodedBitCount = tfg.getNumChars("decoded.txt") * BITS_PER_CHAR ;
-        System.out.println("Decoded bit count (should be same as above): " + decodedBitCount);
+        System.out.printf("Decoded bit count (should be same as original): %,d \n", decodedBitCount);
 
         // Encoded bit count
         encodedBitCount = tfg.getNumChars("encoded.txt") ;
-        System.out.println("Encoded bit count (should be smaller than above): " + encodedBitCount);
+        System.out.printf("Encoded bit count (should be smaller than original): %,d \n", encodedBitCount);
 
         // Percentage output
-        System.out.println("Percentage of compression is: " + (originalBitCount/encodedBitCount * 100));
+        System.out.println("Percentage of compression is: " + (originalBitCount/encodedBitCount * 100) + "%");
 
-    }
-}
+    } // end main
+} // end class
