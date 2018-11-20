0798c4a2eeb3778b5087bb5ce4d4869ebbbcc2e0
Zsuzsanna Dianovics <31230409+deverzs@users.noreply.github.com>
2018-11-20 12:04:43 -0800

    Update HuffmanTree.java
    updated documentation

--- src/edu/miracosta/cs113/HuffmanTree.java
+++ src/edu/miracosta/cs113/HuffmanTree.java
@@ -7,19 +7,24 @@ import java.util.Queue;
 import java.util.Scanner;
 
 /**
- * Class to represent and build a Huffman tree.
+ * Class to represent and build a Huffman tree. The Huffman Tree will accept a String to create the tree,
+ * and use the tree to encode and decode the String used to create the tree.
+ * @author  Dianovics
  */
 public class HuffmanTree implements Serializable,  HuffmanInterface  {
 
     private BinaryTree<HuffNode> huffTree ; // root of the priority queue
     private int[] table ; // frequency table
     private HuffNode[] symbols ; // array of HufNodes
-    private  int arraySize ;
-    private  String[] codeTable ;
+    private int arraySize ;
+    private String[] codeTable ;
     private StringBuilder codes = new StringBuilder() ;
-    private static final int MAX_FILE_SYMBOLS = 70 ,
-                             MAX_ASCII_SYMBOLS = 256 ;
+    private static final int MAX_ASCII_SYMBOLS = 256 ;
 
+    /**
+     * Constructor to create the tree
+     * @param input Input is the String used to create the compression tree
+     */
     public HuffmanTree(String  input) {
         // Creating frequency table
         table = new int[MAX_ASCII_SYMBOLS] ;
@@ -42,15 +47,23 @@ public class HuffmanTree implements Serializable,  HuffmanInterface  {
                 symbolCount++ ;
             }
         }
+        // build the tree
         buildTree(symbols);
+
+        // create the code table
         buildCodeTable(huffTree, "", 0);
+
+        // Translate code table to searchable array
         setToTableCodes();
-
-
     }
 
 
-
+    /**
+     * Helper method to build the code table
+     * @param tree The Binary Tree used to create the Huffman tree
+     * @param s The string used to build the tree
+     * @param arrayCount The count for the array to store the codes
+     */
     private  void buildCodeTable(BinaryTree<HuffNode> tree, String s, int arrayCount) {
 
         String temp ;
@@ -65,20 +78,26 @@ public class HuffmanTree implements Serializable,  HuffmanInterface  {
         }
     }
 
-        private void setToTableCodes(){
-            codeTable = new String[arraySize] ;
+    /**
+     * Helper method that uses the codeString to generate the codeTable[]
+     */
+    private void setToTableCodes(){
+         codeTable = new String[arraySize] ;
 
-            String codeString = codes.toString() ;
-            Scanner fromString = new Scanner(codeString).useDelimiter(",") ;
-            for (int j = 0  ; j < arraySize ; j++) {
-                if (fromString.hasNext()) {
-                    codeTable[j] = fromString.next();
-                }
+         String codeString = codes.toString() ;
+         Scanner fromString = new Scanner(codeString).useDelimiter(",") ;
+         for (int j = 0  ; j < arraySize ; j++) {
+            if (fromString.hasNext()) {
+                codeTable[j] = fromString.next();
             }
+         }
+    }
 
-        }
-
-       public void buildTree(HuffNode[] symbols) {
+    /**
+     * Builds the Huffman Tree using the HuffNode[] array for symbols
+     * @param symbols The array of HuffNodes to build into a tree
+     */
+    public void buildTree(HuffNode[] symbols) {
         Queue<BinaryTree<HuffNode>> theQueue = new PriorityQueue<BinaryTree<HuffNode>>(arraySize, new CompareHuffmanTrees()) ;
 
         for (HuffNode nextSymbol : symbols) {
@@ -139,12 +158,14 @@ public class HuffmanTree implements Serializable,  HuffmanInterface  {
             }
 
         }
-
         return sb.toString() ;
-
     }
 
     ////////////////  COMPARATOR CLASS ////////////////////////////////
+    /**
+     * Class that implements the Comparator to create a compare(). The compare()
+     * uses the weight of the node to prioritize
+     */
     private static class CompareHuffmanTrees implements Comparator<BinaryTree<HuffNode>> {
 
         @Override
@@ -157,15 +178,24 @@ public class HuffmanTree implements Serializable,  HuffmanInterface  {
 
 
     ///////////////////    HUFFNODE CLASS //////////////////////////
+    /**
+     * Class of nodes to be used for Huffman Tree
+     */
     public static class HuffNode implements  Serializable {
         private double weight ;
         private Character symbol ;
 
+        /**
+         * Constructor for HuffNodes
+         * @param weight The frequency of the count for the symbol
+         * @param symbol The char that is being counted
+         */
         public HuffNode(double weight, Character symbol) {
             this.weight = weight ;
             this.symbol = symbol ;
         }
 
+        @Override
         public String toString() {
             return " " + this.symbol ;
         }
